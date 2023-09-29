985f0ffc9db50aa81d1b1d4cfc1980ff9f04d666
yuji <117886384+manusiasukacoding@users.noreply.github.com>
2023-09-29 09:45:48 +0700

    Membuat Login Register

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+//API route untuk user
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+
+//Melindungi Rute
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    // Rute API untuk pengguna logout
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+    Route::get('/get-presensi',  [App\Http\Controllers\API\PresensiController::class, 'getPresensis']);
+
+    Route::post('save-presensi', [App\Http\Controllers\API\PresensiController::class, 'savePresensi']);
 });
