18b1ec496ba78ea0c2d2e2ddefc158e65dd62cda
anggadian <anggadiansap@gmail.com>
2023-10-12 11:24:40 +0700

    perubahan yang besar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        DB::table('users')->insert([
+            'name' => 'Admin Presensi',
+            'username' => 'admin',
+            'email' => 'admin@default.com',
+            'password' => Hash::make('password123'),
+        ]);
     }
 }
