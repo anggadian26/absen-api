69eb11ca679863ad3913d7a3e0e72763b6721eaf
liviananadinesuprapto <117886384+manusiasukacoding@users.noreply.github.com>
2023-11-07 13:53:54 +0700

    Merubah Tampilan Bos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -81,12 +81,20 @@
     </div>
     </nav>
 
-    <main class="py-4">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Login') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6 bg-login-image">
+                                <img src="{{asset('asset/images/presensi.png')}}" alt="" class="login-image">
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">{{ __('LOGIN') }}</h1>
+                                    </div>
 
                         <div class="card-body">
                             <form method="POST" action="{{ route('login-action') }}">
@@ -141,8 +149,8 @@
 
                                 <div class="row mb-0">
                                     <div class="col-md-8 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Login') }}
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            {{ __('Login Akun') }}
                                         </button>
 
                                         @if (Route::has('password.request'))
@@ -161,6 +169,42 @@
 
     </main>
     </div>
+    <style>
+        login-image {
+        width: 100%;
+        height: auto;
+    }
+
+    .h4 {
+    color: #6f42c1;
+    font-weight: bold;
+}
+
+.btn-primary {
+    background-color: #6f42c1; /* Ganti warna latar belakang tombol menjadi ungu */
+    border-color: #6f42c1; /* Ganti warna border tombol menjadi ungu */
+    color: #fff; /* Ganti warna teks tombol menjadi putih */
+}
+
+.btn-primary:hover {
+    background-color: #563d7c; /* Ganti warna latar belakang tombol hover menjadi ungu tua */
+    border-color: #563d7c; /* Ganti warna border tombol hover menjadi ungu tua */
+    color: #fff; /* Tetapkan warna teks tombol hover menjadi putih */
+}
+
+
+.form-control {
+    border: none; /* Menghilangkan border pada input */
+    box-shadow: 0 6px 12px rgba(111, 66, 193, 0.3), 0 0 0 2px rgba(111, 66, 193, 0.2); /* Mengubah bayangan input menjadi ungu dan membuatnya lebih tebal */
+}
+
+.invalid-feedback {
+    color: #6f42c1; /* Mengubah warna teks feedback menjadi ungu */
+}
+
+
+
+    </style>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
@@ -171,3 +215,4 @@
 </body>
 
 </html>
+
